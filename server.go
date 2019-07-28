e5dce1ee161b30e623b17b55878a2b665dd4f59a
Shaun Cains <shaun.cains@acsye.com>
2019-07-29 00:25:41 +0100

    fix: type to Int for port

--- server.go
+++ server.go
@@ -5,13 +5,28 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/vasyahuyasa/july/opds"
 )
 
+func getPort() int {
+	s := os.Getenv("PORT")
+	if s != "" {
+		p, err := strconv.Atoi(s)
+			if err != nil {
+			// handle error
+			fmt.Println(err)
+			os.Exit(2)
+			}
+		return p 
+	}
+	return 8080
+}
+
 func main() {
 	rootDir := flag.String("d", "", "Root storage directory")
-	port := flag.String("p", os.Getenv("PORT"), "Service Port")
+	port := flag.Int("p", getPort(), "Service port")
 	host := flag.String("i", "0.0.0.0", "Service network interface")
 	flag.Parse()
 
